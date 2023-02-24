f2a9b87603f95210223fd5a94afd6fcb131a0788
Semih DAGHAN <semih.daghan@inooster.com>
2023-02-24 11:32:55 +0300

    -- Shared Class Library -- DTOs nesneleri oluşturuldu

---
+++ Services/Catalog/FreeCourse.Services.Catalog/DTOs/CourseCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.DTOs
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public Feature Feature { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
